353a57ddaded8436f95a31ccf0293b5cf3716207
wira <wheerdam@gmail.com>
2016-12-13 20:00:26 +0700

    client

--- src/org/bbi/tools/net/TCPHolePunchClient.java
+++ src/org/bbi/tools/net/TCPHolePunchClient.java
@@ -21,6 +21,7 @@ import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketAddress;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import org.bbi.tools.Log;
@@ -35,12 +36,14 @@ public class TCPHolePunchClient {
     private ServerSocket p2pListener;
     private Socket holePunchServer;
     private Socket p2pClient;
+    private boolean p2pClientConnected = false;
     private int sharedPort;
     private boolean quit = false;
     private long randomID;
     private ExecutorService pool;
     private String[] clientsInfo;
-    public static final int RETRY_DELAY_MS = 1000;
+    
+    public static final long RETRY_DELAY_MS = 1000;
     
     public void connect(String host, int port) {
         pool = Executors.newFixedThreadPool(8);
@@ -92,11 +95,17 @@ public class TCPHolePunchClient {
         pool.shutdown();
     }
     
+    public Socket getP2PClient() {
+        return p2pClient;
+    }
+    
     class PunchHoleServerHandler implements Runnable {
         @Override
         public void run() {
             Log.d(0, "console mode");
             String[] tokens;
+            int numOfConnectedClients, clientID, tryCount;
+            long remoteClientID;
             BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
             String d;
             try {
@@ -107,9 +116,70 @@ public class TCPHolePunchClient {
                     // initiate a p2p connection with a remote host
                     if(d.startsWith("connect")) {
                         tokens = d.trim().split("\\s+");
+                        clientID = Integer.parseInt(tokens[1]);
+                        if(clientID < 0 || clientID > clientsInfo.length) {
+                            break;
+                        }
+                        tokens = clientsInfo[clientID].split("\\s+");
+                        remoteClientID = Long.parseLong(tokens[0], 16);
+                        tokens[2] = tokens[2].substring(6);
+                        InetSocketAddress remoteAddress = new InetSocketAddress(
+                                tokens[1].split(":")[0],
+                                Integer.parseInt(tokens[1].split(":")[1])
+                        );
+                        InetSocketAddress privateAddress = new InetSocketAddress(
+                                tokens[2].split(":")[0],
+                                Integer.parseInt(tokens[2].split(":")[1])
+                        );
+                        // let's check if our remote host happens to be in
+                        // the same NAT as us
+                        Socket s = new Socket();
+                        s.setReuseAddress(true);
+                        s.bind(new InetSocketAddress(sharedPort));
+                        try {
+                           
+                            s.connect(privateAddress);
+                            d = recv(s);
+                            if(d.equals("magix!client " + 
+                                    String.format("%08X", remoteClientID))) {
+                                // success!
+                                p2pClientConnected = true;
+                                p2pClient = s;
+                            }
+                        } catch(IOException ioe) {
+                            // no good
+                        }
+                        tryCount = 0;
+                        while(!quit && !p2pClientConnected) {
+                            Socket remoteSocket = new Socket();
+                            s.setReuseAddress(true);
+                            s.bind(new InetSocketAddress(sharedPort));
+                            try {
+                                tryCount++;
+                                Log.d(0, tryCount + ": connecting to " + 
+                                         remoteAddress.getAddress().getHostAddress() +
+                                         ":" + remoteAddress.getPort());
+                                s.connect(remoteAddress);
+                                d = recv(s);
+                                if(d.equals("magix!client " + 
+                                        String.format("%08X", remoteClientID))) {
+                                    // success!
+                                    p2pClientConnected = true;
+                                    p2pClient = remoteSocket;
+                                }
+                            } catch(IOException ioe) {
+                                // no good
+                                Log.d(0, "failed: " + ioe);
+                            }
+                            try {
+                                Thread.sleep(RETRY_DELAY_MS);
+                            } catch(InterruptedException ie) {
+                                
+                            }
+                        }
                     }
                     send(holePunchServer, d);
-                    int numOfConnectedClients = Integer.parseInt(recv(holePunchServer));
+                    numOfConnectedClients = Integer.parseInt(recv(holePunchServer));
                     clientsInfo = new String[numOfConnectedClients];
                     for(int i = 0; i < numOfConnectedClients; i++) {
                         clientsInfo[i] = recv(holePunchServer);
@@ -133,6 +203,7 @@ public class TCPHolePunchClient {
                 p2pListener.bind(new InetSocketAddress(sharedPort));
                 while(!quit) {
                     p2pClient = p2pListener.accept();
+                    p2pClientConnected = true;
                     InetSocketAddress addr = (InetSocketAddress) p2pClient.getRemoteSocketAddress();
                     Log.d(0, "new p2p connection: " +
                             addr.getAddress().getHostAddress() + ":" + addr.getPort());
